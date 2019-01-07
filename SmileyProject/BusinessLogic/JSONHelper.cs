69f71304b6a460c3a27a283dd30e9f2d5e917837
Furkan <furkanaydn67@gmail.com>
2019-01-07 14:55:30 +0300

    Classes Added

---
+++ SmileyProject/BusinessLogic/JSONHelper.cs
@@ -0,0 +1,25 @@
+﻿using SmileyProject.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace SmileyProject.BusinessLogic
+{
+    class JSONHelper
+    {
+
+        public List<Smiley> GetSmileys()
+        {
+            List<Smiley> smileyList = new List<Smiley>();
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            string content = File.ReadAllText("smiley_content.json");
+            smileyList = jss.Deserialize<List<Smiley>>(content);
+            return smileyList;
+        }
+
+    }
+}
---
+++ SmileyProject/Models/Smiley.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmileyProject.Models
+{
+    class Smiley
+    {
+        public string Name { get; set; }
+        public string Art { get; set; }
+        public Categories Category { get; set; }
+    }
+}
